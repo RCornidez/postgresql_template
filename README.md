21d26863fe7ea734e9642105630afc0276544c8a
RCornidez <cornidez04@gmail.com>
2023-04-14 02:09:38 +0000

    updated README.md, setup_tables.sql, drop_tables.sql

--- README.md
+++ README.md
@@ -1,52 +1,28 @@
 How to run:
-	install Nodejs:
-		curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-		sudo apt install nodejs
-		node --version
-
-	clone the project from github:
-		git clone yourproject.git
-
-	install dependencies:
-		cd backend/
-		npm install
-
-	install PM2: to watch changes and keep running on reboot
-		sudo npm i pm2 -g
-		cd api/
-		pm2 start index.js --watch
-		pm2 startup
-
-	install Nginx: modify to display api on http, test config and restart service
-		sudo apt install nginx
-		sudo vi /etc/nginx/13/sites-available/default
-			within this file modify server block to match the following:
-    	
-			server_name _;
-
-			location / {
-			        proxy_pass http://localhost:3001;
-			        proxy_http_version 1.1;
-			        proxy_set_header Upgrade $http_upgrade;
-			        proxy_set_header Connection 'upgrade';
-			        proxy_set_header Host $host;
-			        proxy_cache_bypass $http_upgrade;
-			}			
-
-		sudo nginx -t
-		sudo service nginx restart
+	visualize this table on drawsql.app
+		link: https://drawsql.app/teams/rodrigos-team-2/diagrams/meetup
 
 	install PostgreSQL:
 		sudo apt-get install postgresql
+	
+	clone the project from github:
 
-		create database:
-			psql -U postgres -d postgres -c 'CREATE DATABASE countability'
+	https version:
+		git clone https://github.com/RCornidez/postgresql_template.git
 
-		create tables:
-			psql -U postgres -d countability -a -f ./backend/database/setup_tables.sql
+	ssh version:
+		git clone git@github.com:RCornidez/postgresql_template.git
 
-		seed test data:
-			psql -U postgres -d countability -a -f ./backend/database/seed_tables.sql
+	navigate within the folder and run the following commands:
 
-		drop tables:
-			psql -U postgres -d countability -a -f ./backend/database/drop_tables.sql
+		create the database:
+			psql -U postgres -d postgres -c 'CREATE DATABASE results'
+
+		create the table:
+			psql -U postgres -d results -a -f ./setup_tables.sql
+
+		seed test data into table:
+			psql -U postgres -d results -a -f ./seed_tables.sql
+
+		delete table from database:
+			psql -U postgres -d results -a -f ./drop_tables.sql
--- setup_tables.sql
+++ setup_tables.sql
@@ -6,125 +6,17 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TABLE IF NOT EXISTS classes (
+
+CREATE TABLE IF NOT EXISTS results (
     id serial PRIMARY KEY,
-    type VARCHAR ( 100 ),
+	user_id int UNIQUE,
+	color VARCHAR ( 100 ),
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE IF NOT EXISTS behaviors (
-    id serial PRIMARY KEY,
-    title VARCHAR ( 100 ) NOT NULL,
-    comment VARCHAR ( 255 ),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE IF NOT EXISTS templates (
-    id serial PRIMARY KEY,
-    title VARCHAR ( 100 ) NOT NULL,
-    duration int,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE IF NOT EXISTS subjects (
-    id serial PRIMARY KEY,
-    first_name VARCHAR ( 100 ),
-    last_name VARCHAR ( 100 ),
-    comment VARCHAR ( 255 ),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE IF NOT EXISTS sessions (
-    id serial PRIMARY KEY,
-    title VARCHAR ( 100 ),
-    environment VARCHAR ( 100 ),
-    comment VARCHAR ( 255 ),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE IF NOT EXISTS bridge_behaviors_classes (
-    id serial PRIMARY KEY,
-    behavior_id int REFERENCES behaviors (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    class_id int REFERENCES classes (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE IF NOT EXISTS bridge_sessions_behaviors_classes (
-    id serial PRIMARY KEY,
-    session_id int REFERENCES sessions (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    behavior_class_id int REFERENCES bridge_behaviors_classes (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    count int,
-    start_time TIMESTAMP,
-    stop_time TIMESTAMP,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-	
-CREATE TABLE IF NOT EXISTS bridge_templates_behaviors_classes (
-    id serial PRIMARY KEY,
-    template_id int REFERENCES templates (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    behavior_class_id int REFERENCES bridge_behaviors_classes (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE IF NOT EXISTS bridge_sessions_subject (
-    id serial PRIMARY KEY,
-    session_id int REFERENCES sessions (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    subject_id int REFERENCES subjects (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    UNIQUE(session_id)
-);
-
 
 CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON classes 
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON behaviors 
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON templates 
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON subjects
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON sessions
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON bridge_behaviors_classes
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON bridge_sessions_behaviors_classes
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON bridge_templates_behaviors_classes
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON bridge_sessions_subject
+BEFORE UPDATE ON results 
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
